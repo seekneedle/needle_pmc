f50103eede30635bb68878e87a7bf9028ce9e3a3
Zoe.ZhaoTJ <zoe.zhaotj@homecreditcfc.cn>
2024-10-12 00:12:13 -0400

    mask

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,7 @@
 from .server import start_server
 import os
+from config import config
 
-os.environ['OPENAI_API_KEY'] = 'sk-8fasEGsTJ6rL78NJE1F6D92bFa764aDaB7D67a15A93dC7A0'
-os.environ['OPENAI_API_BASE'] = 'http://10.26.9.148:3007/v1'
-os.environ['OPENAI_BASE_URL'] = 'http://10.26.9.148:3007/v1'
+os.environ['OPENAI_API_KEY'] = config['api_key']
+os.environ['OPENAI_API_BASE'] = config['base_url']
+os.environ['OPENAI_BASE_URL'] = config['base_url']
